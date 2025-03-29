1d966f5605db441bc9f8b58db939d852cc4482cc
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2025-03-29 19:22:18 +0530

    Admin.js Face

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -9,7 +9,7 @@ const { updateGoogleSheets } = require("../utils/updateGoogleSheets");
 const router = express.Router();
 
 // Correct Python path
-const pythonPath = path.join(__dirname, "../.venv/Scripts/python3.exe");
+const pythonPath = path.join(__dirname, "../.venv/bin/python3");
 console.log(`Using Python Path: ${pythonPath}`);
 
 // Ensure `uploads` directory exists
